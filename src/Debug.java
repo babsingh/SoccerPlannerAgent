9700000b79c2f0ec8f99405c4e08b23d1179e095
Babneet Singh <singhbabneet@gmail.com>
2016-06-10 01:18:52 +0100

    Fixing bugs and adding comments

--- src/Debug.java
+++ src/Debug.java
@@ -1,6 +1,12 @@
+/*
+ * This class prints debug information to stdout.
+ * Printing debug information can be turned on and off via this class.
+ * For better performance, debugging will be turned off during competition.
+ */
 public class Debug {
 	public static boolean enabledDebugging = true;
 
+	/* Print debug information */
 	public static void print(String string) {
 		if (enabledDebugging) {
 			System.out.println(string);
