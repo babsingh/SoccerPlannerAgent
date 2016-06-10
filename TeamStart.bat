4d9c613f7096108edc7fba098c4035e368c81def
Babneet Singh <singhbabneet@gmail.com>
2016-06-10 19:10:16 +0100

    Starts 2 vs 2 agents. Two agents use the planner and other two agents use the original implementation

--- TeamStart.bat
+++ TeamStart.bat
@@ -1,22 +1,23 @@
+REM > out_.log 2>&1
 chdir bin
-start java Krislet -team Carleton
+start java Krislet -team Carleton -mode modified
 ping localhost
-start java Krislet -team Carleton
+start java Krislet -team Carleton -mode modified
 ping localhost
-start java Krislet -team Carleton
+REM start java Krislet -team Carleton -mode modified
+REM ping localhost
+REM start java Krislet -team Carleton -mode modified
+REM ping localhost
+REM start java Krislet -team Carleton -mode modified
+REM ping localhost
+start java Krislet -team University -mode original
 ping localhost
-start java Krislet -team Carleton
+start java Krislet -team University -mode original
 ping localhost
-start java Krislet -team Carleton
-ping localhost
-start java Krislet -team University
-ping localhost
-start java Krislet -team University
-ping localhost
-start java Krislet -team University
-ping localhost
-start java Krislet -team University
-ping localhost
-start java Krislet -team University
-ping localhost
+REM start java Krislet -team University -mode original
+REM ping localhost
+REM start java Krislet -team University -mode original
+REM ping localhost
+REM start java Krislet -team University -mode original
+REM ping localhost
 chdir ..
