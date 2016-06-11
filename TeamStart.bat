96a4701967507b1481b5d6c65f70f66703c90420
Babneet Singh <singhbabneet@gmail.com>
2016-06-11 04:50:57 +0100

    Updating start script

--- TeamStart.bat
+++ TeamStart.bat
@@ -1,23 +1,23 @@
-REM > out_.log 2>&1
+REM > out.log 2>&1
 chdir bin
 start java Krislet -team Carleton -mode modified
 ping localhost
 start java Krislet -team Carleton -mode modified
 ping localhost
-REM start java Krislet -team Carleton -mode modified
-REM ping localhost
-REM start java Krislet -team Carleton -mode modified
-REM ping localhost
-REM start java Krislet -team Carleton -mode modified
-REM ping localhost
+start java Krislet -team Carleton -mode modified
+ping localhost
+start java Krislet -team Carleton -mode modified
+ping localhost
+start java Krislet -team Carleton -mode modified
+ping localhost
+start java Krislet -team University -mode original
+ping localhost
 start java Krislet -team University -mode original
 ping localhost
 start java Krislet -team University -mode original
 ping localhost
-REM start java Krislet -team University -mode original
-REM ping localhost
-REM start java Krislet -team University -mode original
-REM ping localhost
-REM start java Krislet -team University -mode original
-REM ping localhost
+start java Krislet -team University -mode original
+ping localhost
+start java Krislet -team University -mode original
+ping localhost
 chdir ..
